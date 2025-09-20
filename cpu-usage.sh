c219a644c16ce919956b8bde1c1e1309054a0795
JClient0ps <jc.igot@academy.opswerks.com>
2025-09-21 01:04:21 +0800

    Added the cpu-usage script

---
+++ cpu-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CPU_UTIL=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}' | awk '{printf "%.0f\n", $1}')
+TIMESTAMP=$(date ' +%Y-%m-%d_$H:%M:%S')
+LOG_FILE="/var/log/cpu-monitor.log"
+if [ "$CPU_UTIL" -lt 80 ]; then
+	STATUS="OK"
+	EXIT_CODE=0
+elif [ "$CPU_UTIL" -lt 90 ]; then
+	STATUS="WARNING"
+        EXIT_CODE=1
+else
+	STATUS="CRITICAL"
+	EXIT_CODE=2
+fi
+echo "$TIMESTAMP - $STATUS - ${CPU_UTIL}%" >> "$LOG_FILE"
+exit $EXIT_CODE
